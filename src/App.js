e210e2cb58290bef475a82eb0b8f9faf8bebc5b8
spiralspree <spiralspree@gmail.com>
2022-06-27 20:21:24 +0200

    Fetch and show data from backend on Button click

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button';
 
 function App() {
   return (
@@ -17,6 +18,10 @@ function App() {
         >
           Learn React
         </a>
+
+        {/* Testing server communication */}
+        <Button></Button>
+        
       </header>
     </div>
   );
---
+++ src/components/Button.jsx
@@ -0,0 +1,18 @@
+import axios from 'axios'
+import React, {useState, useEffect} from 'react'
+
+export default function Button() {
+  const [buttonText, setButtonText] = useState("Button")
+  const url = "https://wannajoin-backend-test.herokuapp.com/"
+
+  const fetchFromBackend = () => {
+    axios.get(url)
+    .then(response => {
+      setButtonText(response.data)}
+    )
+  }
+  
+  return (
+    <div onClick={fetchFromBackend}>{buttonText}</div>
+  )
+}
